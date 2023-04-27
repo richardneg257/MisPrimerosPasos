4c8451ee4d6e44639712d971b8e9eaf0702f8f05
Richard Negron <richard.negron.257@gmail.com>
2023-04-27 01:28:46 -0500

    creamos alumnoscontroller listar alumnos e inyectamos application y repository en el program

---
+++ MisPrimerosPasos.Api/Controllers/AlumnosController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MisPrimerosPasos.Application.Interfaces;
+using MisPrimerosPasos.Entity;
+
+namespace MisPrimerosPasos.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class AlumnosController : ControllerBase
+{
+    private readonly IAlumnoApplication _alumnoApplication;
+
+    public AlumnosController(IAlumnoApplication alumnoApplication)
+    {
+        _alumnoApplication = alumnoApplication;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Alumno>>> ListarAlumnos()
+    {
+        var alumnos = await _alumnoApplication.ListarAlumnos();
+        return alumnos;
+    }
+}
