6ba31be0b356df7f64ed9366022c6714f2544f6e
Richard Negron <richard.negron.257@gmail.com>
2023-04-27 00:57:10 -0500

    creamos interfaz e implementacion de alumno application

---
+++ MisPrimerosPasos.Application/Implementaciones/AlumnoApplication.cs
@@ -0,0 +1,20 @@
+﻿using MisPrimerosPasos.Application.Interfaces;
+using MisPrimerosPasos.Entity;
+using MisPrimerosPasos.Repository.Interfaces;
+
+namespace MisPrimerosPasos.Application.Implementaciones;
+public class AlumnoApplication : IAlumnoApplication
+{
+    private readonly IAlumnoRepository _alumnoRepository;
+
+    public AlumnoApplication(IAlumnoRepository alumnoRepository)
+    {
+        _alumnoRepository = alumnoRepository;
+    }
+
+    public async Task<List<Alumno>> ListarAlumnos()
+    {
+        var alumnos = await _alumnoRepository.ListarAlumnos();
+        return alumnos;
+    }
+}
