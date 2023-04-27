b8b27109189e4d4301a2b61beceb2c985f9f553c
Richard Negron <richard.negron.257@gmail.com>
2023-04-26 23:20:45 -0500

    creamos la entidad Alumno

---
+++ MisPrimerosPasos.Entity/Alumno.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MisPrimerosPasos.Entity;
+public class Alumno
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    [StringLength(8)]
+    public string Dni { get; set; }
+    [Required]
+    [StringLength(50)]
+    public string Nombres { get; set; }
+    [Required]
+    [StringLength(50)]
+    public string Apellidos { get; set; }
+    [Required]
+    public DateTime FechaNacimiento { get; set; }
+    [Required]
+    public bool Sexo { get; set; } // 0: Femenino, 1: Masculino
+}
